9fa52ba3e8b7a33eccb7ecfe71db1f7db69449a9
PRODIMEI <israelrpm@gmail.com>
2026-01-16 05:05:33 -0600

    Change title to 'Tabla de la Jornada 2'

--- billar-js/script-tabla-liga1.js
+++ billar-js/script-tabla-liga1.js
@@ -36,7 +36,7 @@ if (titulo) {
     if (numeroJornada) {
         titulo.textContent = `Tabla de la Jornada ${numeroJornada}`;
     } else {
-        titulo.textContent = "Tabla de la Jornada 1";
+        titulo.textContent = "Tabla de la Jornada 2";
     }
 }
 
