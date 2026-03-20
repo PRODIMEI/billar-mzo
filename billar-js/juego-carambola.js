af0e8c0ba53b0c23e7c51ee8d58e9378a3b890de
PRODIMEI <israelrpm@gmail.com>
2026-03-20 04:42:56 -0600

    Update juego-carambola.js

--- billar-js/juego-carambola.js
+++ billar-js/juego-carambola.js
@@ -433,7 +433,7 @@ function shoot() {
     const angle = Math.atan2(dy, dx);
 
     //const power = Math.min(pullDistance * 0.12, 10);
-    const power = Math.min(pullDistance * 0.26, 22);
+    const power = Math.min(pullDistance * 0.25, 20);
 
     whiteBall.dx = Math.cos(angle) * power;
     whiteBall.dy = Math.sin(angle) * power;
